5cd8002e2c3bc00d203a40398e76b29fcc60f21e
b9007204 <b9007204@my.shu.ac.uk>
2021-01-12 19:28:58 +0000

    Added StudentsPage Ilist without searchbar

--- Swimclub.Mobile/Swimclub.Mobile/Views/StudentsPage.xaml.cs
+++ Swimclub.Mobile/Swimclub.Mobile/Views/StudentsPage.xaml.cs
@@ -10,11 +10,28 @@ using Xamarin.Forms.Xaml;
 namespace Swimclub.Mobile.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class StudentsPage : ContentPage
-    {
+    public partial class StudentsPage : ContentPage {
+        public IList<Student> Ilist { get; set; }
         public StudentsPage()
         {
+            Ilist = new List<Student>();
+            Ilist.Add(new Student { StudentName = "Joe Bloggs", StudentGrade = "Grade: 1", StudentSwimEnglandNumber = "12345678" });
+            Ilist.Add(new Student { StudentName = "Micheal Phelps", StudentGrade = "Grade: 1", StudentSwimEnglandNumber = "87654321" });
+            Ilist.Add(new Student { StudentName = "Alove Floats", StudentGrade = "Grade: 2", StudentSwimEnglandNumber = "67284153" });
+            Ilist.Add(new Student { StudentName = "Ivanha swim", StudentGrade = "Grade: 2", StudentSwimEnglandNumber = "06417324" });
+            Ilist.Add(new Student { StudentName = "Channel Crosser", StudentGrade = "Grade: 3", StudentSwimEnglandNumber = "14285097" });
+            Ilist.Add(new Student { StudentName = "Speedboat McGee", StudentGrade = "Grade: 3", StudentSwimEnglandNumber = "14285097" });
+
             InitializeComponent();
+
+            Coll1.ItemsSource = Ilist;
         }
+    }
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public string StudentGrade { get; set; }
+        public string StudentSwimEnglandNumber { get; set; }
+
     }
 }
