a5f81dcf17791eebd7c87a0ed35e5c1cf433ed5f
b9007204 <b9007204@my.shu.ac.uk>
2021-01-12 23:21:33 +0000

    Added a working search bar to StudentPage

--- Swimclub.Mobile/Swimclub.Mobile/Views/StudentsPage.xaml.cs
+++ Swimclub.Mobile/Swimclub.Mobile/Views/StudentsPage.xaml.cs
@@ -26,6 +26,14 @@ namespace Swimclub.Mobile.Views
 
             Coll1.ItemsSource = Ilist;
         }
+
+        private void StudentPageSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchResultName = Ilist.Where(c => c.StudentName.ToLower().Contains(StudentSearchBar.Text.ToLower()));
+            Coll1.ItemsSource = searchResultName;
+
+            
+        }
     }
     public class Student
     {
