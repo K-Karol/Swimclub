11c0bc2126dfc2f75d841bd2c7bcc630015db283
JamesWannell <jwannell99@gmail.com>
2021-01-13 10:48:24 +0000

    updated UIs

--- Swimclub.Mobile/Swimclub.Mobile/Models/Item.cs
+++ Swimclub.Mobile/Swimclub.Mobile/Models/Item.cs
@@ -7,5 +7,9 @@ namespace Swimclub.Mobile.Models
 		public string Id { get; set; }
 		public string Text { get; set; }
 		public string Description { get; set; }
+
+		public string studentID = "Smith";
+
+		public string studentName = "000000";
 	}
 }
